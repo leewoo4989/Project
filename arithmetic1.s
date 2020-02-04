30e8a7a36b7330fc762869846b4f2a0859f13c9b
leewoo4989 <leewoo4989@gmail.com>
2020-02-04 15:17:06 -0500

    Added comments to the program

--- arithmetic1.s
+++ arithmetic1.s
@@ -3,13 +3,13 @@
   .section .text
   .globl _start
   _start:
-    mov r1,#10
-    mov r2,#11
-    mov r3,#7
-    mov r4,#2
+    mov r1,#10 @load r1 with 10
+    mov r2,#11 @load r2 with 11
+    mov r3,#7  @load r3 with 7
+    mov r4,#2  @load r4 with 2
 
-    add r1,r1,r2
-    mul r3,r3,r4
-    sub r1,r1,r3
-    svc #0
+    add r1,r1,r2  @add r2 to r1 and store it in r1
+    mul r3,r3,r4  @add r4 to r3 and store it in r3
+    sub r1,r1,r3  @add r3 to r1 and store it in r1
+    svc #0        
   .end
